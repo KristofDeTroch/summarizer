3b6485b1da4d7b2afb699c02093373d17bc77870
kristofdetroch <detroch.kristof@gmail.com>
2024-06-21 15:47:08 +0200

    fix: store token

--- index.ts
+++ index.ts
@@ -14,17 +14,42 @@ const auth = Google({
   scope:
     "openid https://www.googleapis.com/auth/calendar.readonly https://www.googleapis.com/auth/calendar.events.readonly",
 });
-async function storeToken(res: oauth.OpenIDTokenEndpointResponse) {
-  await Bun.write(FILE_PATH, JSON.stringify(res));
+async function storeToken(token: oauth.OpenIDTokenEndpointResponse) {
+  const createdAt = new Date().toISOString();
+  await Bun.write(FILE_PATH, JSON.stringify({ token, createdAt }));
+}
+
+async function isTokenExpired() {
+  try {
+    const { token, createdAt } = await Bun.file(FILE_PATH).json();
+    const now = new Date().getTime();
+    const created = new Date(createdAt).getTime();
+    const diff = now - created;
+    if (diff > (token.expires_in - 60)! * 1000) {
+      return true;
+    }
+  } catch (e) {
+    return true;
+  }
+  return false;
+}
+async function getToken() {
+  const { token } = await Bun.file(FILE_PATH).json();
+
+  return token;
 }
 
 const callBackUri = new URL("callback", address);
 const { state, url } = await auth.authorize(callBackUri.toString());
-console.log(`open the url in the browser '${url}'`);
-if (os.platform() === "darwin") {
-  Bun.spawn(["open", url.toString()]);
+if (await isTokenExpired()) {
+  if (os.platform() === "darwin") {
+    Bun.spawn(["open", url.toString()]);
+  } else {
+    console.log(url.toString());
+  }
 } else {
-  console.log(url.toString());
+  const url = new URL("dashboard", address);
+  Bun.spawn(["open", url.toString()]);
 }
 
 Bun.serve({
@@ -44,11 +69,18 @@ Bun.serve({
         },
       });
     }
+    if (request.url.includes("dashboard")) {
+      return new Response(Bun.file("html/index.html"), {
+        headers: {
+          "Content-Type": "text/html",
+        },
+      });
+    }
     if (request.url.includes("generate")) {
       const { searchParams } = new URL(request.url);
       const since = searchParams.get("since") + "T00:00:00Z";
       const until = searchParams.get("until") + "T23:59:59Z";
-      const token = await Bun.file(FILE_PATH).json();
+      const token = await getToken();
       const data = await startGeneration({ since, until }, token);
 
       const template = await constructTemplate(data!);
